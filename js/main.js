752e22b96abc522ae4fe1785bb23f33e975f1edf
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-03-04 14:12:50 +0100

    working JS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const btn = document.querySelector('.card__contentTwo__form__btn');
 const name = document.querySelector('#name');
-const email = document.querySelector('#mail');
+const email = document.querySelector('#email');
+const emailErrorText = document.querySelector('#emailErrorText');
 const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const checkEmail = () => {
 	if (email.value.match(reg)) {
+		emailErrorText.innerHTML = "<br>"
 		console.log('ok');
 	} else {
 		console.log('nie jest ok');
+		emailErrorText.innerHTML = "Looks like this is not an email"
 	}
 };
 
